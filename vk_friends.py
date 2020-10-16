ba6c75d5918b4763bd1445a6cb2f28fb85da516c
Nataly Makhrova <panicpanic@yandex.ru>
2020-10-16 15:22:04 +0300

    Changed logic

--- vk_friends.py
+++ vk_friends.py
@@ -18,32 +18,17 @@ class VKUser:
         self.first_name = ''
         self.last_name = ''
         self.user_link = 'http://www.vk.com/id' + str(self.user_id)
+        self.get_user_info()
+
+    def __create_method_url(self, method):
+        return urljoin(self.BASE_URL, method)
 
     def __and__(self, other):
         res = self.get_mutual_friends(other.user_id)
         return res
 
     def __str__(self):
-        self.get_user_info()
         return f'{self.first_name} {self.last_name} {self.user_link}'
-
-    def __create_method_url(self, method):
-        return urljoin(self.BASE_URL, method)
-
-    def get_mutual_friends(self, target_user_id):
-        res = requests.get(self.__create_method_url('friends.getMutual'), params={
-            'access_token': self.token,
-            'v': self.version,
-            'source_uid': self.user_id,
-            'target_uid': target_user_id
-        })
-        friends_list = []
-        for elm in res.json()['response']:
-            friend = VKUser(elm)
-            friend.get_user_info()
-            friends_list.append(friend)
-            time.sleep(1)
-        return friends_list
 
     def get_user_info(self):
         res = requests.get(self.__create_method_url('users.get'), params={
@@ -59,10 +44,26 @@ class VKUser:
             user_info = ''
         return user_info
 
+    def get_mutual_friends(self, target_user_id):
+        res = requests.get(self.__create_method_url('friends.getMutual'), params={
+            'access_token': self.token,
+            'v': self.version,
+            'source_uid': self.user_id,
+            'target_uid': target_user_id
+        })
+        friends_list = []
+        for elm in res.json()['response']:
+            friend = VKUser(elm)
+            friends_list.append(friend)
+            time.sleep(1)
+        return friends_list
+
+
 if __name__ == '__main__':
     user1 = VKUser(2359003)
     user2 = VKUser(2116146)
     print(user1)
+    print(user2)
 
     mutual_friends_list = user1 & user2
     i = 0
